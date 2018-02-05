660d9054a8270cb4862273173a38002588620bef
Petar Mari? <petar.maric@gmail.com>
2018-02-05 12:48:49 +0100

    Added the `run_analyses_on_all_experiments` Fabric task, along with the 'fsm_modal_analysis' analysis type
    It's responsible for running all supported types of analyses on all of the experiments

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,21 @@ from fabric.api import local, task
 import tables as tb
 
 
+ANALYSES_TYPES = {
+    'fsm_modal_analysis': {
+        'program_args_fmt': "%(results_file)s --report_file %(report_file)s",
+        'report_file_ext': 'pdf',
+        'variations': [
+            {},
+            {'a-max': 1000.0,},
+            {'a-max': 1500.0,},
+            {'t_b-min': 4.0, 't_b-max': 8.0,},
+            {'t_b-min': 4.0, 't_b-max': 8.0, 'a-min': 2000.0,},
+        ],
+    },
+}
+
+
 def find_filenames(top_dir, filename_pattern):
     for path, _, file_list in os.walk(top_dir):
         for name in fnmatch.filter(file_list, filename_pattern):
@@ -34,3 +49,38 @@ def compute_experiment(data_file, force=False):
 def compute_all_experiments(top_dir='.', force=False):
     for data_file in find_filenames(top_dir, '*.yaml'):
         compute_experiment(data_file, force=force)
+
+
+@task
+def run_single_analysis_type(results_file, analysis_type):
+    reports_dir = os.path.join(os.path.dirname(results_file), analysis_type)
+    reports_base_name = os.path.join(reports_dir, os.path.splitext(os.path.basename(results_file))[0])
+
+    if not os.path.exists(reports_dir):
+        os.mkdir(reports_dir)
+
+    analysis_settings = ANALYSES_TYPES[analysis_type]
+    for variation_dict in analysis_settings['variations']:
+        sorted_variation_items = sorted(variation_dict.items())
+
+        variation_filename_part = ','.join("%s=%s" % (k, v) for k, v in sorted_variation_items)
+        report_file = "%s%s.%s" % (
+            reports_base_name,
+            '@' + variation_filename_part if variation_filename_part else '',
+            analysis_settings['report_file_ext']
+        )
+
+        program_args = analysis_settings['program_args_fmt'] % locals()
+        variation_program_args = ' '.join("--%s=%s" % (k, v) for k, v in sorted_variation_items)
+        local("%s %s %s" % (analysis_type, program_args, variation_program_args))
+
+@task
+def run_analyses(results_file, force_analysis_type=''):
+    analyses_types = ANALYSES_TYPES if not force_analysis_type else (force_analysis_type,)
+    for analysis_type in analyses_types:
+        run_single_analysis_type(results_file, analysis_type)
+
+@task
+def run_analyses_on_all_experiments(top_dir='.', force_analysis_type=''):
+    for results_file in find_filenames(top_dir, '*.hdf5'):
+        run_analyses(results_file, force_analysis_type=force_analysis_type)
