c00bffcf2c1fc3609353237e780ec5ee2c1f132a
Petar Marić <petar.maric@gmail.com>
2018-03-21 14:47:08 +0100

    Added the 'fsm_strip_thickness_analysis' analysis type

--- fabfile.py
+++ fabfile.py
@@ -32,6 +32,15 @@ ANALYSES_TYPES = {
             ],
         },
     },
+    'fsm_strip_thickness_analysis': {
+        'program_args_fmt': "%(results_file)s --report_file %(report_file)s",
+        'report_file_ext': 'pdf',
+        'variations': {
+            'barbero/*.hdf5': [
+                {'a': 2310, 'add-automatic-markers': '',},
+            ],
+        },
+    },
 }
 
 
